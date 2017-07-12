23de5789b829a0ae52a77d2d97f71225f96c2417
vincenthou <vincenthou365@gmail.com>
2017-07-13 07:33:29 +0800

    refactor(home): use grid layout and add travis-ci

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ -d docs ]; then
+    echo "➥ Commit files"
+    git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/${REPO_SLUG}.git ${REPO_SLUG} > /dev/null
+    cp -fr docs/* ${REPO_SLUG}
+    cd ${REPO_SLUG}
+    ls -la
+    git config user.email "travis@travis-ci.org"
+    git config user.name "travis-ci"
+    git add -A
+    git commit -m "[ci skip] publish website"
+    git push -fq origin gh-pages > /dev/null
+else
+    echo '➥ Fail'
+    exit 1
+fi
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -5,13 +5,21 @@
       <mu-circular-progress :size="40"/>
     </div>
     <div class="movies-wrapper">
-      <mu-grid-list cell-height="570">
-        <mu-grid-tile v-for="movie, index in movies" :key="index">
-          <img :src="movie.poster">
-          <span slot="title">{{movie.title}}</span>
-          <span slot="subTitle">{{movie.year}} - {{movie.tag}} - {{movie.type}}</span>
-        </mu-grid-tile>
-      </mu-grid-list>
+      <mu-row gutter class="movie-row" v-for="cols, rowIndex in movieRows" :key="rowIndex">
+        <mu-col width="100" tablet="50" desktop="25" v-for="movie, colIndex in cols" :key="colIndex">
+          <mu-card>
+            <mu-card-media :title="movie.title" :subTitle="movie.type">
+              <img height=500 :src="movie.poster">
+            </mu-card-media>
+            <mu-card-actions>
+              <mu-chip>{{movie.year}}</mu-chip>
+              <mu-chip>{{movie.tag}}</mu-chip>
+              <mu-text-field disabled="true" v-model="path" fullWidth v-for="path, pathIndex in movie.paths" :key="pathIndex"/>
+              <mu-raised-button label="原始详情页面" target="__blank" :href="movie.link" primary/>
+            </mu-card-actions>
+          </mu-card>
+        </mu-col>
+      </mu-row>
     </div>
   </div>
 </template>
@@ -19,13 +27,16 @@
 <!-- Add "scoped" attribute to limit CSS to this page only -->
 <style scoped>
   .movies-wrapper {
-    margin: 10px auto;
-    width: 800px;
+    margin: 20px 10px;
+  }
+  .movie-row {
+    padding-bottom: 10px; 
   }
 </style>
 
 <script>
 import axios from 'axios'
+const MOVIE_COL = 4
 
 export default {
   data () {
@@ -43,12 +54,22 @@ export default {
   },
   methods: {
     fetchData () {
-      this.error = this.movies = null
+      this.error = this.movieRows = null
       this.loading = true
       axios.get('/static/latest.json')
         .then(resp => {
           this.loading = false
-          this.movies = resp.data
+          console.log(resp.data)
+          let rows = resp.data.reduce((rows, item, index, items) => {
+            let rowIndex = ~~(index / MOVIE_COL)
+            if (index % MOVIE_COL) {
+              rows[rowIndex].push(item)
+            } else {
+              rows[rowIndex] = [item]
+            }
+            return rows
+          }, [])
+          this.movieRows = rows
         })
         .catch(error => {
           if (error) {
